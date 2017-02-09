cc4222048b7d6b8a277cc5bd1ce930aa2f9c65b4
Olivia Stalcup <ocstalcup@gmail.com>
2017-02-08 19:40:45 -0800

    click buttons for gold

---
+++ server.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, redirect, request, session
+import random
+app = Flask(__name__)
+app.secret_key = "whatEverYouWantToCallIt"
+
+@app.route('/')
+def index():
+	if "gold" not in session:
+		session["gold"] = 0 
+		
+	return render_template('index.html')
+
+@app.route('/process_money', methods=["POST"])
+def process_money():
+	if request.form["building"] == "farm": 
+		addgold = random.randrange(10,20)
+		session["gold"] += addgold 
+	if request.form["building"] == "cave":
+		addgold = random.randrange(5,10)
+		session["gold"] += addgold
+	if request.form["building"] == "house":
+		addgold = random.randrange(2,5)
+		session["gold"] += addgold
+	if request.form["building"] == "casino":
+		addgold = random.randrange(-50,50)
+		session["gold"] += addgold
+	return redirect('/')
+
+app.run(debug=True)
